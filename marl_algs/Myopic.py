fixed averaging in Myopic

marl_algs/Myopic.py
@@ -168,16 +168,15 @@ def Greedy(env, p_signal, K, tracked_agent, name, n_steps):
             action_every_agent[i].append(actions[i])
 
             shannon_cap_cum_current_step += best_cap #np.log2(1 + SINR)
-            
-        den = max(1, int(active_users.sum()))
         
-        coverage.append(sum_transmitted/den)
+        
+        coverage.append(sum_transmitted/len(active_users))
             
         avg_delay_this_slot = tracker.end_slot()
             
         # step summaries
-        shannon_cap_cum_all.append(shannon_cap_cum_current_step/den)
-        cumulative_cost_q_all.append(cumulative_cost_q_current_step/den)
+        shannon_cap_cum_all.append(shannon_cap_cum_current_step/len(active_users))
+        cumulative_cost_q_all.append(cumulative_cost_q_current_step/len(active_users))
         
         
     delay = tracker.avg_delay_per_slot